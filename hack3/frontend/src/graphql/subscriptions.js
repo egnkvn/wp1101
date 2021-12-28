da9ab08ccce4b3918bb21fc845f81c25a1cce83e
unknown <b09902021@ntu.edu.tw>
2021-12-28 12:08:23 +0800

    wqe

--- hack3/frontend/src/graphql/subscriptions.js
+++ hack3/frontend/src/graphql/subscriptions.js
@@ -23,7 +23,7 @@ export const TASK_UPDATED_SUBSCRIPTION = gql`
 
 // TODO 6.4 Uncomment the following line and finish gql part
 export const TASK_DELETED_SUBSCRIPTION = gql`
-  subscription OnTaskCreated {
+  subscription OnTaskDeleted {
     taskDeleted {
       id
     }
