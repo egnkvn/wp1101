66f44034d63c33534c9df807d143a4d3cf37f626
unknown <b09902021@ntu.edu.tw>
2021-12-28 11:28:32 +0800

    test

--- hack3/backend/src/resolvers/Mutation.js
+++ hack3/backend/src/resolvers/Mutation.js
@@ -33,6 +33,13 @@ const Mutation = {
    * Delete a task by id
    */
   // TODO 5.2 Add a deleteTask function to resolve deleteTask
+  deleteTask: async (parent, { id }, { taskModel, pubSub }) => {
+    const task = await taskModel.findOneAndRemove({ id }, { returnDocument: "after" });
+    pubSub.publish("TASK_DELETED", {
+      taskDeleted: task,
+    });
+    return id;
+  }
   // TODO 6.3 Add Subscription
 };
 
